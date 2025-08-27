ae8f0241ed192e488c632ddb81963095ee7ce5b6
Silvester van der Leer <=>
2025-08-27 03:07:10 +0200

    Fix HTML syntax error - remove stray > character in teams.php edit button

--- php_interface/teams.php
+++ php_interface/teams.php
@@ -162,7 +162,7 @@ ob_start();
                                     </td>
                                     <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
                                         <div class="flex justify-end space-x-2">
-                                            <button @click="editTeam(<?php echo htmlspecialchars(json_encode($team)); ?>)" class="text-water-blue-600 hover:text-water-blue-900" title="<?php echo $team['name'] === 'H1/H2' ? t('edit_team_h1h2_tooltip') : t('edit_team_tooltip'); ?>">>
+                                            <button @click="editTeam(<?php echo htmlspecialchars(json_encode($team)); ?>)" class="text-water-blue-600 hover:text-water-blue-900" title="<?php echo $team['name'] === 'H1/H2' ? t('edit_team_h1h2_tooltip') : t('edit_team_tooltip'); ?>">
                                                 <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
                                                 </svg>
